8a09b17fb1ce488a4a66f9234342fc1af9812c14
coding67-KR <a51286652@gmail.com>
2026-09-06 12:16:52 +0900

    Add Chicken Defense Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace = "com.coding67.chickendefense"; compileSdk = 36
+    defaultConfig { applicationId = "com.coding67.chickendefense"; minSdk = 24; targetSdk = 36; versionCode = 1; versionName = "1.0" }
+}
